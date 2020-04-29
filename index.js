1224bd398fa79a2342e63c22c5d8362ddf2ecb68
abdomashael <abdomashael@yahoo.com>
2020-04-30 01:50:05 +0200

    project skeleton

---
+++ index.js
@@ -0,0 +1,36 @@
+const express = require("express")
+const cors = require("cors")
+const bodyParser = require("body-parser")
+const mongoose = require('mongoose')
+
+const app = express()
+
+const PORT = 3000
+
+
+app.use(cors())
+app.use(bodyParser.json())
+
+mongoose.connect("mongodb://localhost:27017/amiiboa", {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    })
+    .then(() => {
+        console.log("db is Conncted")
+
+    })
+    .catch(err => console.log(err))
+
+app.use((req, res,next) => {
+    //logger for all requests on server endpoints
+    console.log(`${new Date()} ---- ${req.method} ---- ${req.url} `);
+    
+    next()
+})
+
+
+app.listen(PORT, (err) => {
+    if (!err) {
+        console.log(`Server Listen on ${PORT}`)
+    }
+})
