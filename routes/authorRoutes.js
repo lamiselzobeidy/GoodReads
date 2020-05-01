3e30d7eee728fa7c92f3c6f2ea90faeb76aa5ca2
Omaure <omarakram958@gmail.com>
2020-05-01 03:55:49 +0200

    adding comments

--- routes/authorRoutes.js
+++ routes/authorRoutes.js
@@ -36,7 +36,7 @@ router.post("/", upload.single('authorImage'), async (req, res) => {
 
 router.get("/", async (req, res) => {
     try {
-        // Here we need to check the JWT token before getting all categories
+        // Here we need to check the JWT token before getting all authors
         allAuthors = await authorModel.find({});
         //202 means accepted
         res.status(202).json(allAuthors);
@@ -48,7 +48,7 @@ router.get("/", async (req, res) => {
 
 router.get("/:id", async (req, res) => {
     try {
-        // Here we need to check the JWT token before getting a category
+        // Here we need to check the JWT token before getting a author
         const author = await authorModel.findById(req.params.id);
         //202 means accepted
         res.status(202).json(author);
@@ -60,7 +60,7 @@ router.get("/:id", async (req, res) => {
 
 router.patch("/:id", async (req, res) => {
     try {
-        // Here we need to check the JWT token before updating a category
+        // Here we need to check the JWT token before updating a author
         const authorData = req.body;
         const editedAuthor = await authorModel.updateOne({_id: req.params.id}, authorData);
         //202 means accepted
@@ -73,7 +73,7 @@ router.patch("/:id", async (req, res) => {
 
 router.delete("/:id", async (req, res) => {
     try {
-        // Here we need to check the JWT token before deleting a category
+        // Here we need to check the JWT token before deleting a author
         const deleteAuthor = await authorModel.deleteOne({_id: req.params.id});
         //202 means accepted
         res.status(202).json(deleteAuthor.nModified);
