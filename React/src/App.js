7b92e2e226a75b76083fd01ccbca4fe5e3852a9d
fadyjan <fady.jan95@gmail.com>
2020-05-04 02:43:11 +0200

    Nav bar Component

---
+++ React/src/App.js
@@ -0,0 +1,16 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import MainNavBar from './components/MainNavBar/mainNavBar'
+
+function App() {
+  return (
+    <div className="App">
+        <MainNavBar></MainNavBar>
+    </div>
+  );
+}
+
+export default App;
---
+++ React/src/components/MainNavBar/mainNavBar.js
@@ -0,0 +1,45 @@
+import React from 'react'
+import {Navbar,Button , Nav , Form,FormControl} from 'react-bootstrap/';
+
+
+
+
+const MainNavBar = () => {
+    return (
+        
+            
+  <Navbar bg="dark" variant="dark">
+        <Navbar.Brand href="#home">
+      <img
+        src="https://p7.hiclipart.com/preview/142/76/939/symbol-yellow-orange-logo-ibooks.jpg"
+        width="30"
+        height="30"
+        className="d-inline-block align-top"
+        alt="React Bootstrap logo"
+      />
+    </Navbar.Brand>
+    <Nav className="mr-auto">
+      <Nav.Link href="#home">Home</Nav.Link>
+      <Nav.Link href="#features">Authors</Nav.Link>
+      <Nav.Link href="#pricing">Categories</Nav.Link>
+    </Nav>
+    <Form inline>
+      <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+      <Button variant="outline-info">Search</Button>
+      <Form.Label className="mx-3">User name</Form.Label>
+      <img
+        src="https://p7.hiclipart.com/preview/142/76/939/symbol-yellow-orange-logo-ibooks.jpg"
+        width="30"
+        height="30"
+        className="d-inline-block align-top ml-2"
+        alt="React Bootstrap logo"
+      />
+    </Form>
+  </Navbar>
+       
+
+    );
+
+}
+
+export default MainNavBar
