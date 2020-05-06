b1cf6af1023ae8e7ccce181332f5ad1a4b79766e
abdomashael <abdomashael@yahoo.com>
2020-05-06 03:13:01 +0200

    add book Bio to model

--- models/authorModel.js
+++ models/authorModel.js
@@ -5,6 +5,7 @@ let authorSchema = new mongoose.Schema({
     firstName: {type: String, required: true},
     lastName: {type: String, required: true},
     authorImage: {type: String},
+    bio: {type: String},
     DateofBirth: {type: Date,default:Date.now},
 });
 
