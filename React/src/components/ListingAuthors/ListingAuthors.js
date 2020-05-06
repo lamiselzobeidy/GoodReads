e7ec3a6cb5b07afbb7f4f83d2cfdc26b46a52c72
fadyjan <fady.jan95@gmail.com>
2020-05-06 07:46:12 +0200

    update on LitingAuthor component

--- React/src/components/ListingAuthors/ListingAuthors.js
+++ React/src/components/ListingAuthors/ListingAuthors.js
@@ -1,189 +1,205 @@
 import React from 'react'
-import { Card, Image, Button } from 'semantic-ui-react'
-import "./ListingAuthors.css"
+import './test.css'
+import { Card, Image, Button } from 'semantic-ui-react';
 
-const src = 'https://react.semantic-ui.com/images/wireframe/white-image.png'
+var i = 0;
+const colorPicker = () => {
+    
+    const colors = ["yellow", "green", "red",
+         "blue", "red", "orange", "olive", "teal", "pink",
+        "black", "violet"]
+         i= i+1;
+
+         return colors[i]
+}
+
 const ListingAuthors = () => (
-    <Card.Group itemsPerRow={6} >
-        <Card >
-            <Card.Content>
-                <Card.Header>Steve Sanders</Card.Header>
-                <Card.Meta>Friends of Elliot</Card.Meta>
-                <Card.Description>
-                    Steve wants to add you to the group <strong>best friends</strong>
-                </Card.Description>
-            </Card.Content>
-            <Card.Content extra>
-                <div className='ui two buttons'>
-                    <Button basic color='green'>
-                        Show Profile
+
+    <div className='hi' >
+        <p className="paragraphs">All Authors</p>
+        <Card.Group itemsPerRow={5} >
+            <Card  >
+                <Card.Content>
+                    <Card.Header  >Steve Sanders</Card.Header>
+                    <Card.Meta>Friends of Elliot</Card.Meta>
+                    <Card.Description>
+                        Steve wants to add you to the group <strong>best friends</strong>
+                    </Card.Description>
+                </Card.Content>
+                <Card.Content extra>
+                    <div className='ui two buttons'>
+                        <Button basic color={colorPicker()}>
+                            Show Profile
           </Button>
-                </div>
-            </Card.Content>
-        </Card>
-        <Card >
-            <Card.Content>
-                <Card.Header>Steve Sanders</Card.Header>
-                <Card.Meta>Friends of Elliot</Card.Meta>
-                <Card.Description>
-                    Steve wants to add you to the group <strong>best friends</strong>
-                </Card.Description>
-            </Card.Content>
-            <Card.Content extra>
-                <div className='ui two buttons'>
-                    <Button basic color='green'>
-                        Show Profile
+                    </div>
+                </Card.Content>
+            </Card>
+            <Card >
+                <Card.Content>
+                    <Card.Header>Steve Sanders</Card.Header>
+                    <Card.Meta>Friends of Elliot</Card.Meta>
+                    <Card.Description>
+                        Steve wants to add you to the group <strong>best friends</strong>
+                    </Card.Description>
+                </Card.Content>
+                <Card.Content extra>
+                    <div className='ui two buttons'>
+                        <Button basic color={colorPicker()}>
+                            Show Profile
           </Button>
-                </div>
-            </Card.Content>
-        </Card>
-        <Card >
-            <Card.Content>
-                <Card.Header>Steve Sanders</Card.Header>
-                <Card.Meta>Friends of Elliot</Card.Meta>
-                <Card.Description>
-                    Steve wants to add you to the group <strong>best friends</strong>
-                </Card.Description>
-            </Card.Content>
-            <Card.Content extra>
-                <div className='ui two buttons'>
-                    <Button basic color='green'>
-                        Show Profile
+                    </div>
+                </Card.Content>
+            </Card>
+            <Card >
+                <Card.Content>
+                    <Card.Header>Steve Sanders</Card.Header>
+                    <Card.Meta>Friends of Elliot</Card.Meta>
+                    <Card.Description>
+                        Steve wants to add you to the group <strong>best friends</strong>
+                    </Card.Description>
+                </Card.Content>
+                <Card.Content extra>
+                    <div className='ui two buttons'>
+                        <Button basic color={colorPicker()}>
+                            Show Profile
           </Button>
-                </div>
-            </Card.Content>
-        </Card>
-        <Card >
-            <Card.Content>
-                <Card.Header>Steve Sanders</Card.Header>
-                <Card.Meta>Friends of Elliot</Card.Meta>
-                <Card.Description>
-                    Steve wants to add you to the group <strong>best friends</strong>
-                </Card.Description>
-            </Card.Content>
-            <Card.Content extra>
-                <div className='ui two buttons'>
-                    <Button basic color='green'>
-                        Show Profile
+                    </div>
+                </Card.Content>
+            </Card>
+            <Card >
+                <Card.Content>
+                    <Card.Header>Steve Sanders</Card.Header>
+                    <Card.Meta>Friends of Elliot</Card.Meta>
+                    <Card.Description>
+                        Steve wants to add you to the group <strong>best friends</strong>
+                    </Card.Description>
+                </Card.Content>
+                <Card.Content extra>
+                    <div className='ui two buttons'>
+                        <Button basic color={colorPicker()}>
+                            Show Profile
           </Button>
-                </div>
-            </Card.Content>
-        </Card>
-        <Card >
-            <Card.Content>
-                <Card.Header>Steve Sanders</Card.Header>
-                <Card.Meta>Friends of Elliot</Card.Meta>
-                <Card.Description>
-                    Steve wants to add you to the group <strong>best friends</strong>
-                </Card.Description>
-            </Card.Content>
-            <Card.Content extra>
-                <div className='ui two buttons'>
-                    <Button basic color='green'>
-                        Show Profile
+                    </div>
+                </Card.Content>
+            </Card>
+            <Card >
+                <Card.Content>
+                    <Card.Header>Steve Sanders</Card.Header>
+                    <Card.Meta>Friends of Elliot</Card.Meta>
+                    <Card.Description>
+                        Steve wants to add you to the group <strong>best friends</strong>
+                    </Card.Description>
+                </Card.Content>
+                <Card.Content extra>
+                    <div className='ui two buttons'>
+                        <Button basic color={colorPicker()}>
+                            Show Profile
           </Button>
-                </div>
-            </Card.Content>
-        </Card>
-        <Card >
-            <Card.Content>
-                <Card.Header>Steve Sanders</Card.Header>
-                <Card.Meta>Friends of Elliot</Card.Meta>
-                <Card.Description>
-                    Steve wants to add you to the group <strong>best friends</strong>
-                </Card.Description>
-            </Card.Content>
-            <Card.Content extra>
-                <div className='ui two buttons'>
-                    <Button basic color='green'>
-                        Show Profile
+                    </div>
+                </Card.Content>
+            </Card>
+            <Card >
+                <Card.Content>
+                    <Card.Header>Steve Sanders</Card.Header>
+                    <Card.Meta>Friends of Elliot</Card.Meta>
+                    <Card.Description>
+                        Steve wants to add you to the group <strong>best friends</strong>
+                    </Card.Description>
+                </Card.Content>
+                <Card.Content extra>
+                    <div className='ui two buttons'>
+                        <Button basic color={colorPicker()}>
+                            Show Profile
           </Button>
-                </div>
-            </Card.Content>
-        </Card>
-        <Card >
-            <Card.Content>
-                <Card.Header>Steve Sanders</Card.Header>
-                <Card.Meta>Friends of Elliot</Card.Meta>
-                <Card.Description>
-                    Steve wants to add you to the group <strong>best friends</strong>
-                </Card.Description>
-            </Card.Content>
-            <Card.Content extra>
-                <div className='ui two buttons'>
-                    <Button basic color='green'>
-                        Show Profile
+                    </div>
+                </Card.Content>
+            </Card>
+            <Card >
+                <Card.Content>
+                    <Card.Header>Steve Sanders</Card.Header>
+                    <Card.Meta>Friends of Elliot</Card.Meta>
+                    <Card.Description>
+                        Steve wants to add you to the group <strong>best friends</strong>
+                    </Card.Description>
+                </Card.Content>
+                <Card.Content extra>
+                    <div className='ui two buttons'>
+                        <Button basic color={colorPicker()}>
+                            Show Profile
           </Button>
-                </div>
-            </Card.Content>
-        </Card>
-        <Card >
-            <Card.Content>
-                <Card.Header>Steve Sanders</Card.Header>
-                <Card.Meta>Friends of Elliot</Card.Meta>
-                <Card.Description>
-                    Steve wants to add you to the group <strong>best friends</strong>
-                </Card.Description>
-            </Card.Content>
-            <Card.Content extra>
-                <div className='ui two buttons'>
-                    <Button basic color='green'>
-                        Show Profile
+                    </div>
+                </Card.Content>
+            </Card>
+            <Card >
+                <Card.Content>
+                    <Card.Header>Steve Sanders</Card.Header>
+                    <Card.Meta>Friends of Elliot</Card.Meta>
+                    <Card.Description>
+                        Steve wants to add you to the group <strong>best friends</strong>
+                    </Card.Description>
+                </Card.Content>
+                <Card.Content extra>
+                    <div className='ui two buttons'>
+                        <Button basic color={colorPicker()}>
+                            Show Profile
           </Button>
-                </div>
-            </Card.Content>
-        </Card>
-        <Card >
-            <Card.Content>
-                <Card.Header>Steve Sanders</Card.Header>
-                <Card.Meta>Friends of Elliot</Card.Meta>
-                <Card.Description>
-                    Steve wants to add you to the group <strong>best friends</strong>
-                </Card.Description>
-            </Card.Content>
-            <Card.Content extra>
-                <div className='ui two buttons'>
-                    <Button basic color='green'>
-                        Show Profile
+                    </div>
+                </Card.Content>
+            </Card>
+            <Card >
+                <Card.Content>
+                    <Card.Header>Steve Sanders</Card.Header>
+                    <Card.Meta>Friends of Elliot</Card.Meta>
+                    <Card.Description>
+                        Steve wants to add you to the group <strong>best friends</strong>
+                    </Card.Description>
+                </Card.Content>
+                <Card.Content extra>
+                    <div className='ui two buttons'>
+                        <Button basic color={colorPicker()}>
+                            Show Profile
           </Button>
-                </div>
-            </Card.Content>
-        </Card>
-        <Card >
-            <Card.Content>
-                <Card.Header>Steve Sanders</Card.Header>
-                <Card.Meta>Friends of Elliot</Card.Meta>
-                <Card.Description>
-                    Steve wants to add you to the group <strong>best friends</strong>
-                </Card.Description>
-            </Card.Content>
-            <Card.Content extra>
-                <div className='ui two buttons'>
-                    <Button basic color='green'>
-                        Show Profile
+                    </div>
+                </Card.Content>
+            </Card>
+            <Card >
+                <Card.Content>
+                    <Card.Header>Steve Sanders</Card.Header>
+                    <Card.Meta>Friends of Elliot</Card.Meta>
+                    <Card.Description>
+                        Steve wants to add you to the group <strong>best friends</strong>
+                    </Card.Description>
+                </Card.Content>
+                <Card.Content extra>
+                    <div className='ui two buttons'>
+                        <Button basic color={colorPicker()}>
+                            Show Profile
           </Button>
-                </div>
-            </Card.Content>
-        </Card>
-        <Card >
-            <Card.Content>
-                <Card.Header>Steve Sanders</Card.Header>
-                <Card.Meta>Friends of Elliot</Card.Meta>
-                <Card.Description>
-                    Steve wants to add you to the group <strong>best friends</strong>
-                </Card.Description>
-            </Card.Content>
-            <Card.Content extra>
-                <div className='ui two buttons'>
-                    <Button basic color='green'>
-                        Show Profile
+                    </div>
+                </Card.Content>
+            </Card>
+            <Card >
+                <Card.Content>
+                    <Card.Header>Steve Sanders</Card.Header>
+                    <Card.Meta>Friends of Elliot</Card.Meta>
+                    <Card.Description>
+                        Steve wants to add you to the group <strong>best friends</strong>
+                    </Card.Description>
+                </Card.Content>
+                <Card.Content extra>
+                    <div className='ui two buttons'>
+                        <Button basic color={colorPicker()}>
+                            Show Profile
           </Button>
-                </div>
-            </Card.Content>
-        </Card>
+                    </div>
+                </Card.Content>
+            </Card>
 
 
-    </Card.Group>
+        </Card.Group>
+    </div>
+
 )
+
 
 export default ListingAuthors
