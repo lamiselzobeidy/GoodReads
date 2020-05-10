b68d4adbdcaa9a1484346eb300025739c10c9655
fadyjan <fady.jan95@gmail.com>
2020-05-10 04:18:39 +0200

    AdminBook Pop UP Modal

--- react0/src/components/AdminPanel/AdminBooks/AdminBooks.js
+++ react0/src/components/AdminPanel/AdminBooks/AdminBooks.js
@@ -1,12 +1,89 @@
 import React from 'react';
-import { Table } from 'react-bootstrap'
+import { Table, Modal, Button, Form, Col, Row } from 'react-bootstrap'
 import '../AdminCatogries/AdminCatogries.css'
 import { Icon } from 'semantic-ui-react';
 
 
+function MyVerticallyCenteredModal(props) {
+    return (
+        <Modal
+            {...props}
+            size="lg"
+            aria-labelledby="contained-modal-title-vcenter"
+            centered
+        >
+            <Modal.Header closeButton>
+                <Modal.Title id="contained-modal-title-vcenter">
+                    ADD Book
+          </Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+                <Form>
+                    <Form.Group as={Row} controlId="formPlaintextPassword">
+                        <Form.Label column sm="2">
+                            Book Name
+                     </Form.Label>
+                        <Col sm="10">
+                            <Form.Control size="lg" type="text" placeholder="Enter Book Name ..." />
+                        </Col>
+                    </Form.Group>
+
+                    <Form.Group as={Row} controlId="formPlaintextPassword">
+                        <Form.Label column sm="2">
+                            Category
+                     </Form.Label>
+                        <Col sm="10">
+                            <Form.Control as="select" value="Choose...">
+                                <option>Choose...</option>
+                                <option>...</option>
+                            </Form.Control>
+                        </Col>
+                    </Form.Group>
+
+                    <Form.Group as={Row} controlId="formPlaintextPassword">
+                        <Form.Label column sm="2">
+                            Author
+                     </Form.Label>
+                        <Col sm="10">
+                            <Form.Control as="select" value="Choose...">
+                                <option>Choose...</option>
+                                <option>...</option>
+                            </Form.Control>
+                        </Col>
+                    </Form.Group>
+
+                    <div className="mb-3">
+                        <Form.File id="formcheck-api-custom" custom>
+                            <Form.File.Input isValid />
+                            <Form.File.Label data-browse="Button text">
+                                Upload Your Image
+                             </Form.File.Label>
+                        </Form.File>
+                    </div>
+
+                </Form>
+
+            </Modal.Body>
+            <Modal.Footer>
+                <Button onClick={props.onHide}>Close</Button>
+                <Button variant="primary" onClick={props.onHide}>
+                    Save Changes
+          </Button>
+            </Modal.Footer>
+        </Modal>
+    );
+}
+
+
+
+
 function AdminBooks() {
+    const [modalShow, setModalShow] = React.useState(false);
     return (
         <div>
+            <a className="iconadjustment" onClick={() => setModalShow(true)}>
+                <Icon name='add circle test' />
+            </a>
             <Table striped bordered hover>
                 <thead>
                     <tr>
@@ -28,17 +105,21 @@ function AdminBooks() {
                         <td>Otto</td>
                         <td><a>
                             <Icon name='edit' />
-                            </a>
+                        </a>
                             <a>
-                            <Icon name='delete' />
+                                <Icon name='delete' />
                             </a>
                         </td>
 
 
                     </tr>
-                  
+
                 </tbody>
             </Table>
+            <MyVerticallyCenteredModal
+                show={modalShow}
+                onHide={() => setModalShow(false)}
+            />
 
         </div>
     );
