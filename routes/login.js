19f92cf1c11f9c8105c79656558e91e6261d0918
abdomashael <abdomashael@yahoo.com>
2020-05-13 04:02:35 +0200

    fix response

--- routes/login.js
+++ routes/login.js
@@ -14,15 +14,22 @@ router.get('/', async (req, res) => {
     try {
         const user = await userModel.find({ email: usermail })
 
+        if (user.length===0) {
+            res.send(401,"Invalid Information")
+        }
         // test a matching password
         user[0].comparePassword(password, async (err,isMatch) => {
             if (err) throw err;
             if (isMatch) {
-                const signToken = await jwt.sign({ usermail: usermail }, "secretkey")
-                user[0].token = signToken
-                //save new Token
-                const result= await userModel.findByIdAndUpdate(user[0]._id,user[0])
-                res.send(signToken)    
+                if (!user[0].token) {
+                    const signToken = await jwt.sign({ usermail: usermail }, "secretkey")
+                    user[0].token = signToken
+                    //save new Token
+                    const result= await userModel.findByIdAndUpdate(user[0]._id,user[0])
+                    res.send({token:signToken,isAdmin:result.isAdmin})        
+                }
+                res.send({token:user[0].token,isAdmin:user[0].isAdmin})        
+                
             }else {
                 res.send(401,"Invalid Information")
             }
@@ -44,6 +51,8 @@ router.get('/', async (req, res) => {
     }
     
 })
+
+
 
 
 module.exports = router
