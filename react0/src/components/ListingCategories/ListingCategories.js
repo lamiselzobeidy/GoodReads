b705a7b21224bc2e67fd2be4195586b396072ec2
fadyjan <fady.jan95@gmail.com>
2020-05-18 10:20:44 +0200

    BackUp Listing Categories
    ListingCategories as a backUp for Lamis Component

---
+++ react0/src/components/ListingCategories/ListingCategories.js
@@ -0,0 +1,117 @@
+import React from "react"
+import { Card, Image, Button, Icon } from 'semantic-ui-react';
+import './ListingCategories.css'
+
+const ListingCategories = () => {
+
+    return (
+
+        <Card.Group itemsPerRow={4} className="container mt-5">
+
+            <Card >
+                <Image src='https://www.booktopia.com.au/blog/wp-content/uploads/2018/12/the-arsonist.jpg' wrapped ui={false} />
+                <Card.Content>
+                    <Card.Header>Daniel</Card.Header>
+                    <Card.Meta>Joined in 2016</Card.Meta>
+                    <Card.Description>
+                        Daniel is a comedian living in Nashville.
+                </Card.Description>
+                </Card.Content>
+                <Card.Content extra>
+                    <a>
+                        <Icon name='book' />
+                        More details
+                     </a>
+                </Card.Content>
+            </Card>
+            <Card>
+                <Image src='https://www.booktopia.com.au/blog/wp-content/uploads/2018/12/the-arsonist.jpg' wrapped ui={false} />
+                <Card.Content>
+                    <Card.Header>Daniel</Card.Header>
+                    <Card.Meta>Joined in 2016</Card.Meta>
+                    <Card.Description>
+                        Daniel is a comedian living in Nashville.
+                </Card.Description>
+                </Card.Content>
+                <Card.Content extra>
+                    <a>
+                        <Icon name='book' />
+                        More details
+                     </a>
+                </Card.Content>
+            </Card>
+            <Card>
+                <Image src='https://www.booktopia.com.au/blog/wp-content/uploads/2018/12/the-arsonist.jpg' wrapped ui={false} />
+                <Card.Content>
+                    <Card.Header>Daniel</Card.Header>
+                    <Card.Meta>Joined in 2016</Card.Meta>
+                    <Card.Description>
+                        Daniel is a comedian living in Nashville.
+                </Card.Description>
+                </Card.Content>
+                <Card.Content extra>
+                    <a>
+                        <Icon name='book' />
+                        More details
+                     </a>
+                </Card.Content>
+            </Card>
+            <Card>
+                <Image src='https://www.booktopia.com.au/blog/wp-content/uploads/2018/12/the-arsonist.jpg' wrapped ui={false} />
+                <Card.Content>
+                    <Card.Header>Daniel</Card.Header>
+                    <Card.Meta>Joined in 2016</Card.Meta>
+                    <Card.Description>
+                        Daniel is a comedian living in Nashville.
+                </Card.Description>
+                </Card.Content>
+                <Card.Content extra>
+                    <a>
+                        <Icon name='book' />
+                        More details
+                     </a>
+                </Card.Content>
+            </Card>
+            <Card>
+                <Image src='https://www.booktopia.com.au/blog/wp-content/uploads/2018/12/the-arsonist.jpg' wrapped ui={false} />
+                <Card.Content>
+                    <Card.Header>Daniel</Card.Header>
+                    <Card.Meta>Joined in 2016</Card.Meta>
+                    <Card.Description>
+                        Daniel is a comedian living in Nashville.
+                </Card.Description>
+                </Card.Content>
+                <Card.Content extra>
+                    <a>
+                        <Icon name='book' />
+                        More details
+                     </a>
+                </Card.Content>
+            </Card>
+            <Card>
+                <Image src='https://www.booktopia.com.au/blog/wp-content/uploads/2018/12/the-arsonist.jpg' wrapped ui={false} />
+                <Card.Content>
+                    <Card.Header>Daniel</Card.Header>
+                    <Card.Meta>Joined in 2016</Card.Meta>
+                    <Card.Description>
+                        Daniel is a comedian living in Nashville.
+                </Card.Description>
+                </Card.Content>
+                <Card.Content extra>
+                    <a>
+                        <Icon name='book' />
+                        More details
+                     </a>
+                </Card.Content>
+            </Card>
+
+        </Card.Group>
+
+
+
+
+    );
+
+}
+
+export default ListingCategories
