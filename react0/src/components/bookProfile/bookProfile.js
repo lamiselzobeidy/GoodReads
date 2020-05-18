a3619b54418c454aa61113cdc0c03c0a84ff284b
Lamis Elzobeidy <lamis.zobeidy@gmail.com>
2020-05-19 01:16:05 +0200

    merged with api

--- react0/src/components/bookProfile/bookProfile.js
+++ react0/src/components/bookProfile/bookProfile.js
@@ -1,55 +1,68 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './bookProfile.css';
 import { Row, Col, Dropdown, Card } from 'react-bootstrap';
-import StarRating from 'react-bootstrap-star-rating';
-import StarRatings from 'react-star-ratings';
-import { MDBContainer, MDBRating } from 'mdbreact';
-import Book from './images/book.png';
+import { MDBRating } from 'mdbreact';
+import axios from 'axios';
 import { Link } from 'react-router-dom';
+import BeautyStars from 'beauty-stars';
 
-const bookProfile = () => {
-    return (
-        <div className="bookProfile pl-5 pt-5 mt-5 ml-5 pr-0">
-            <div className="bookDetails">
-                <Row>
-                    <Col sm={3} className="pr-0" >
-                        <Card style={{ width: '16rem', height:'250px' }}>
-                            <Card.Img variant="top" src={Book} height="120px" className="w-75 mx-auto" />
+const BookProfile = (props) => {
+    const { match: { params } } = props;
+    const [book, setBook] = useState(0);
+    const [stars, setSars] = useState(0);
+    const getBook = () => {
+        // axios.get(`http://34.107.102.252:3000/book/${params.bookId}`)
+        axios.get('http://34.107.102.252:3000/book/5ec2d02f919f0b7d7211addf')
+            .then(result => {
+                setBook(result.data);
+            })
+    }
+    if (book) {
+        return (
+            <div className=" pt-5 bookDetails">
+                <Row className="mx-auto">
+                    <Col xs="4">
+                        <Card className="bookCard mx-auto" >
+                            <Card.Img variant="top" src={book.coverImageName} height="120px" className="w-75 mx-auto mt-3" />
                             <Card.Body className="mx-auto">
-                                <Dropdown className="mx-auto" >
-                                    <Dropdown.Toggle variant="info" id="dropdown-basic" style={{fontSize:'12px'}}>Want to Read</Dropdown.Toggle>
+                                <Dropdown>
+                                    <Dropdown.Toggle variant="info" id="dropdown-basic" style={{ fontSize: '12px' }}>Want to Read</Dropdown.Toggle>
                                     <Dropdown.Menu >
-                                        <Dropdown.Item href="#/action-1" style={{fontSize:'12px'}}>Want to Read</Dropdown.Item>
-                                        <Dropdown.Item href="#/action-2" style={{fontSize:'12px'}}>Currently reading</Dropdown.Item>
-                                        <Dropdown.Item href="#/action-3" style={{fontSize:'12px'}}>Done</Dropdown.Item>
+                                        <Dropdown.Item href="#/action-1" style={{ fontSize: '12px' }}>Want to Read</Dropdown.Item>
+                                        <Dropdown.Item href="#/action-2" style={{ fontSize: '12px' }}>Currently reading</Dropdown.Item>
+                                        <Dropdown.Item href="#/action-3" style={{ fontSize: '12px' }}>Done</Dropdown.Item>
                                     </Dropdown.Menu>
                                 </Dropdown>
-                                <MDBRating className="starRating" iconRegular />
-
+                                <BeautyStars size="20px" value={stars} onChange={value => setSars(value)} />
                             </Card.Body>
                         </Card>
 
                     </Col>
-                    <Col sm={6} className="pl-2">
-                        <h2 className="bookname">Book Name</h2>
-                        <Link>by BookAuthor</Link><br />
-                        <Link>Category Name</Link><br/>
-                        <StarRatings
-                            rating={2.403}  
-                            starDimension="20px"
-                            starSpacing="1px"
-                        /><br/>
-                        <p>This is a brief abou the book, In Harry Potter and the Philosopher's Stone, Harry, an orphan, lives with the Dursleys, his horrible aunt and uncle, and their abominable son, Dudley. One day just before his eleventh birthday, an owl tries to deliver a mysterious letter—the first of a sequence of events that end in Harry meeting a giant man named Hagrid…</p>
+                    <Col xs="5" className="pt-4 bookInfo" >
+                        <h2 className="bookname">{book.bookName}</h2>
+                        <Link>by {book.authorId.firstName} {book.authorId.lastName}</Link><br/>
+                        <Link>{book.catId.categoryName}</Link>
+                        <BeautyStars size="15px" value={stars} onChange={value => setSars(value)} />
+                        <p className="mt-3">This is a brief abou the book, In Harry Potter and the Philosopher's Stone, Harry, an orphan, lives with the Dursleys, his horrible aunt and uncle, and their abominable son, Dudley. One day just before his eleventh birthday, an owl tries to deliver a mysterious letter—the first of a sequence of events that end in Harry meeting a giant man named Hagrid…</p>
+                    </Col>
+
+                </Row>
+                <Row className="justify-content-md-center w-100">
+                    <Col sm={10}>
+                        <div className="mt-5 pt-3">
+                            <h2 className="bookname">Review</h2>
+                            <p>First Review: Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+                        </div>
                     </Col>
                 </Row>
-                    <div className="mt-5 pt-3">
-                    <h2 className="bookname">Review</h2>
-                    <p>First Review: Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
-                    </div>
-
             </div>
-        </div>
-    )
+        )
+    }
+    else {
+        getBook();
+        return null;
+    }
 }
 
-export default bookProfile;
+
+export default BookProfile;
--- react0/src/components/welcomePage/welcomePage.js
+++ react0/src/components/welcomePage/welcomePage.js
@@ -1,76 +1,134 @@
-import React from "react";
+import React, { useState, Component } from "react";
 import "./welcomePage.css";
 import { Navbar, Nav, NavDropdown, Form, Button } from "react-bootstrap";
 import Logo from "../../images/logo.png";
 import FbSignin from "./images/fb_signin.png";
 import TwSignin from "./images/tw_signin.webp";
 import GgleSignin from "./images/google_signin.webp";
+import axios from 'axios';
 
-const welcomePage = () => {
-    return (
-        <div className="WelcomePage">
-            <Navbar id="navbar" variant="dark" className="py-0">
-                <Navbar.Collapse className="justify-content-center" id="logo">
-                    <Navbar.Text className="navlink" href="#home">
-                        <Navbar.Brand href="#home">
-                            <img alt="" src={Logo} width="40" height="40" className="mb-3" />{' '}
-                            <span className="navbarSpan">good<span className="navbarSpan" style={{ fontWeight: "bolder" }}>reads</span></span>
-                        </Navbar.Brand>
-                    </Navbar.Text>
-                </Navbar.Collapse>
-            </Navbar>
-            <div className="container mx-0 px-0 row w-100">
-                <div className="col-7 h-100 py-5" id="quote">
-                    <h1 className="mt-3">Your next book is our mission.</h1>
-                </div>
-                <div className="col-5 h-100 pt-3" style={{ backgroundColor: 'rgba(23, 23, 24, 0.24)' }}>
-                    <Form className="form py-3 px-4 w-75">
-                        <Form.Group controlId="formBasicEmail">
-                            <Form.Label>Email address</Form.Label>
-                            <Form.Control type="email" placeholder="Enter email" />
-                        </Form.Group>
 
-                        <Form.Group controlId="formBasicPassword">
-                            <Form.Label>Password</Form.Label>
-                            <Form.Control type="password" placeholder="Password" />
-                        </Form.Group>
-                        <Form.Group controlId="formBasicCheckbox">
-                            <Form.Check type="checkbox" label="Check me out" />
-                        </Form.Group>
-                        <div id="btn" className="text-center">
-                            <Button variant="primary" type="submit" style={{ backgroundColor: '#247BA0', borderColor: '#247BA0' }}>
-                                Login
-                        </Button>
+class welcomePage extends Component {
+    constructor(props) {
+        super(props)
+        this.state = ({
+            user: undefined,
+            mail: '',
+            pass: '',
+        });
+        this.onChange = this.onChange.bind(this)
+        this.onSubmit = this.onSubmit.bind(this)
+    }
+
+    onChange(e) {
+        this.setState({ [e.target.name]: e.target.value })
+    }
+
+    onSubmit(e) {
+        e.preventDefault()
+        console.log(this.state)
+        axios.post('http://34.107.102.252:3000/login', this.state)
+        .then(result => {
+            if (result.data.error) {
+                console.log("error");
+            }
+            else {
+                this.setState({ user: result.data });
+                localStorage.setItem('User', JSON.stringify(this.state.user));
+                return window.location = "/userprofile"
+            }
+
+        });
+        // axios.request({
+        //     method: 'GET',
+        //     url: `http://abdoking95.tplinkdns.com:3000/login`,
+        //     params: {
+        //         mail: this.state.mail,
+        //         pass: this.state.pass
+        //     },
+
+        // })
+        // axios.post('http://34.107.102.252:3000/login', {
+        //     params: {
+        //         mail: this.state.mail,
+        //         pass: this.state.pass
+        //     }
+        //     })
+        //     .then(function (response) {
+        //     console.log(response.data);
+        //     }) 
+
+        // axios.get('http://abdoking95.tplinkdns.com:3000/login', this.state)
+        //     .then(function (response) {
+        //         console.log(response);
+        //     })
+        //     .catch(function (error) {
+        //         console.log(error);
+        //     });
+    }
+
+    render() {
+
+        return (
+            <div className="WelcomePage">
+                <Navbar id="navbar" variant="dark" className="py-0">
+                    <Navbar.Collapse className="justify-content-center" id="logo">
+                        <Navbar.Text className="navlink" href="#home">
+                            <Navbar.Brand href="#home">
+                                <img alt="" src={Logo} width="40" height="40" className="mb-3" />{' '}
+                                <span className="navbarSpan">good<span className="navbarSpan" style={{ fontWeight: "bolder" }}>reads</span></span>
+                            </Navbar.Brand>
+                        </Navbar.Text>
+                    </Navbar.Collapse>
+                </Navbar>
+                <div className="container mx-0 px-0 row w-100 mt-0">
+                    <div className="col-7 h-100 py-5" id="quote">
+                        <h1 className="mt-3">Your next book is our mission.</h1>
+                    </div>
+                    <div className="col-5 h-100 pt-3" style={{ backgroundColor: 'rgba(23, 23, 24, 0.24)' }}>
+                        <Form className="form py-3 px-4 w-75" noValidate onSubmit={this.onSubmit}>
+                            <Form.Group controlId="formBasicEmail">
+                                <Form.Label>Email address</Form.Label>
+                                <Form.Control className="FormInput" type="email" placeholder="Enter email" name="mail" value={this.state.mail} onChange={this.onChange} />
+                            </Form.Group>
+
+                            <Form.Group controlId="formBasicPassword">
+                                <Form.Label>Password</Form.Label>
+                                <Form.Control className="FormInput" type="password" placeholder="Password" name="pass" value={this.state.pass} onChange={this.onChange} />
+                            </Form.Group>
+                            <div id="btn" className="text-center">
+                                <Button type="submit" style={{ backgroundColor: '#247BA0', borderColor: '#247BA0' }}>Login</Button>
+                            </div>
+                        </Form>
+                        <div className="signinIcons px-4 pt-2 mt-2 w-75">
+                            <span className="signinSpam">or sign in using</span>
+                            <a href=""><img alt="" src={FbSignin} width="27" height="27" className="ml-2" /></a>{'   '}
+                            <a href=""><img alt="" src={TwSignin} width="30" height="30" className="ml-2" /></a>{'   '}
+                            <a href=""><img alt="" src={GgleSignin} width="27" height="27" className="ml-2" /></a>{'   '}
                         </div>
-                    </Form>
-                    <div className="signinIcons px-4 pt-2 mt-2 w-75">
-                                <span className="signinSpam">or sign in using</span>
-                                <a href=""><img alt="" src={FbSignin} width="27" height="27" className="ml-2" /></a>{'   '}
-                                <a href=""><img alt="" src={TwSignin} width="30" height="30" className="ml-2" /></a>{'   '}
-                                <a href=""><img alt="" src={GgleSignin} width="27" height="27" className="ml-2" /></a>{'   '}
                     </div>
                 </div>
+                <div className="about row p-4 mx-0 pb-0 mt-2 w-100">
+                    <div className="col-4 w-100">
+                        <h4>Deciding what to read next?</h4>
+                        <p>You’re in the right place. Tell us what titles or genres you’ve enjoyed in the past, and we’ll give you surprisingly insightful recommendations.</p>
+
+                    </div>
+                    <div className="col-4 w-100">
+                        <h4>What are your friends reading?</h4>
+                        <p>Chances are your friends are discussing their favorite (and least favorite) books on Goodreads.</p>
+
+                    </div>
+                    <div className="col-4 w-100">
+                        <h4>Are you an Author or a Publisher?</h4>
+                        <p>Gain access to a massive audience of more than 90 million book lovers. Goodreads is a great place to promote your books.</p>
+
+                    </div>
+                </div>
+
             </div>
-            <div className="about row p-4 mx-0 pb-0 mt-2 w-100">
-                <div className="col-4 w-100">
-                    <h4>Deciding what to read next?</h4>
-                    <p>You’re in the right place. Tell us what titles or genres you’ve enjoyed in the past, and we’ll give you surprisingly insightful recommendations.</p>
-
-                </div>
-                <div className="col-4 w-100">
-                    <h4>What are your friends reading?</h4>
-                    <p>Chances are your friends are discussing their favorite (and least favorite) books on Goodreads.</p>
-
-                </div>
-                <div className="col-4 w-100">
-                    <h4>Are you an Author or a Publisher?</h4>
-                    <p>Gain access to a massive audience of more than 90 million book lovers. Goodreads is a great place to promote your books.</p>
-
-                </div>
-            </div>
-            
-        </div>
-    );
+        );
+    }
 };
 
 export default welcomePage;
