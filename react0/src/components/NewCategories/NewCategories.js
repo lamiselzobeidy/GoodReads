3bbe9703b8bb310479eb5044517d8fa5b0014186
fadyjan <fady.jan95@gmail.com>
2020-05-18 10:17:39 +0200

    add NewCategories
    Replace the old ListingCategories of me with new one

---
+++ react0/src/components/NewCategories/NewCategories.js
@@ -0,0 +1,133 @@
+import React, { useEffect, useState, } from 'react'
+import './NewCategories.css'
+import axios from 'axios'
+
+
+const NewCategories = () => {
+    const [categories, setCategories] = useState([])
+
+    useEffect(() => {
+        axios.get("http://34.107.102.252:3000/category/")
+            .then(res => {
+                setCategories(res.data);
+            })
+            .catch(err => {
+                console.log(err);
+
+            })
+    }, [])
+
+
+
+    return (
+        <div class="mainc">
+            <a href="gg">
+
+                <div class="card">
+                    <div class="card-image"></div>
+                    <div class="card-text">
+                        <span class="date">4 days ago</span>
+                        <h2>Post One</h2>
+                        <p>Lorem ipsum dolor sit amet consectetur, Ducimus, repudiandae temporibus omnis illum maxime quod deserunt eligendi dolor</p>
+                    </div>
+                    <div class="card-stats">
+                        <div class="stat">
+                            <div class="value">4<sup>m</sup></div>
+                            <div class="type">read</div>
+                        </div>
+                        <div class="stat border">
+                            <div class="value">5123</div>
+                            <div class="type">views</div>
+                        </div>
+                        <div class="stat">
+                            <div class="value">32</div>
+                            <div class="type">comments</div>
+                        </div>
+                    </div>
+                </div>
+            </a>
+
+            <a>
+                <div class="card">
+                    <div class="card-image card2"></div>
+                    <div class="card-text card2">
+                        <span class="date">1 week ago</span>
+                        <h2>Post Two</h2>
+                        <p>Adipisicing elit. Ducimus, repudiandae corrupti amet temporibus omnis provident illum maxime quod. Lorem ipsum dolor</p>
+                    </div>
+                    <div class="card-stats card2">
+                        <div class="stat">
+                            <div class="value">7<sup>m</sup></div>
+                            <div class="type">read</div>
+                        </div>
+                        <div class="stat border">
+                            <div class="value">7152</div>
+                            <div class="type">views</div>
+                        </div>
+                        <div class="stat">
+                            <div class="value">21</div>
+                            <div class="type">comments</div>
+                        </div>
+                    </div>
+                </div>
+            </a>
+
+
+            <a>
+                <div class="card">
+                    <div class="card-image card3"></div>
+                    <div class="card-text card3">
+                        <span class="date">3 week ago</span>
+                        <h2>Post Three</h2>
+                        <p>Repudiandae corrupti amet temporibus omnis provident illum maxime. Ducimus, lorem ipsum dolor adipisicing elit</p>
+                    </div>
+                    <div class="card-stats card3">
+                        <div class="stat">
+                            <div class="value">5<sup>m</sup></div>
+                            <div class="type">read</div>
+                        </div>
+                        <div class="stat border">
+                            <div class="value">3021</div>
+                            <div class="type">views</div>
+                        </div>
+                        <div class="stat">
+                            <div class="value">15</div>
+                            <div class="type">comments</div>
+                        </div>
+                    </div>
+                </div>
+            </a>
+
+            <a>
+
+                <div class="card">
+                    <div class="card-image card4"></div>
+                    <div class="card-text card4">
+                        <span class="date">3 week ago</span>
+                        <h2>Post Three</h2>
+                        <p>Repudiandae corrupti amet temporibus omnis provident illum maxime. Ducimus, lorem ipsum dolor adipisicing elit</p>
+                    </div>
+                    <div class="card-stats card4">
+                        <div class="stat">
+                            <div class="value">5<sup>m</sup></div>
+                            <div class="type">read</div>
+                        </div>
+                        <div class="stat border">
+                            <div class="value">3021</div>
+                            <div class="type">views</div>
+                        </div>
+                        <div class="stat">
+                            <div class="value">15</div>
+                            <div class="type">comments</div>
+                        </div>
+                    </div>
+                </div>
+            </a>
+
+        </div>
+
+    );
+
+}
+
+export default NewCategories
