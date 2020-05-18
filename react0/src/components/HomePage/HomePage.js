67040e01ecca3b5e12004e12d8f8c80a69b7c29e
fadyjan <fady.jan95@gmail.com>
2020-05-18 10:21:25 +0200

    Update HomePage.js

--- react0/src/components/HomePage/HomePage.js
+++ react0/src/components/HomePage/HomePage.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import HotBooks from '../HotBooks/HotBooks'
-import ListingCatogries from '../ListingCatogries/ListingCatogries'
+import ListingCatogries from '../ListingCategories/ListingCategories'
+import { Button} from 'react-bootstrap';
+import NewCategories from '../NewCategories/NewCategories'
 
 
 const HomePage =()=>{
@@ -8,8 +10,10 @@ const HomePage =()=>{
     return(
 
         <div>
+        <Button variant="light" className="btn" style={{fontSize:30}}>Popular books</Button> 
         <HotBooks></HotBooks>
-        <ListingCatogries></ListingCatogries>
+        <NewCategories></NewCategories>
+        {/* <ListingCatogries></ListingCatogries> */}
         </div>
 
 
