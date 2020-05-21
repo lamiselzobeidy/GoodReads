ebc469f13773295e748de362a592d633972d5dc0
Lamis Elzobeidy <lamis.zobeidy@gmail.com>
2020-05-21 06:37:16 +0200

    replaced location storage with session storage

--- react0/src/components/welcomePage/welcomePage.js
+++ react0/src/components/welcomePage/welcomePage.js
@@ -1,31 +1,12 @@
-import React, { useState, Component } from "react";
+import React, { Component } from "react";
 import "./welcomePage.css";
-import {
-  Navbar,
-  Nav,
-  NavDropdown,
-  Form,
-  Button,
-  Tabs,
-  Tab,
-} from "react-bootstrap";
+import {Navbar, Form, Button, Tabs, Tab, } from "react-bootstrap";
 import Logo from "../../images/logo.png";
 import FbSignin from "./images/fb_signin.png";
 import TwSignin from "./images/tw_signin.webp";
 import GgleSignin from "./images/google_signin.webp";
 import axios from "axios";
 import RegistrationPage from "../RegistrationPage/registrationPage";
-import {
-  MDBContainer,
-  MDBRow,
-  MDBCol,
-  MDBCard,
-  MDBCardBody,
-  MDBInput,
-  MDBBtn,
-  MDBIcon,
-  MDBModalFooter,
-} from "mdbreact";
 
 class welcomePage extends Component {
   constructor(props) {
@@ -53,36 +34,11 @@ class welcomePage extends Component {
           console.log("error");
         } else {
           this.setState({ user: result.data });
-          localStorage.setItem("User", JSON.stringify(this.state.user));
-          return (window.location = "/userprofile");
+          sessionStorage.setItem("user",JSON.stringify(this.state.user))
+          console.log(sessionStorage.getItem('user'))
+          return (window.location = `/homepage`);
         }
       });
-    // axios.request({
-    //     method: 'GET',
-    //     url: `http://abdoking95.tplinkdns.com:3000/login`,
-    //     params: {
-    //         mail: this.state.mail,
-    //         pass: this.state.pass
-    //     },
-
-    // })
-    // axios.post('http://34.107.102.252:3000/login', {
-    //     params: {
-    //         mail: this.state.mail,
-    //         pass: this.state.pass
-    //     }
-    //     })
-    //     .then(function (response) {
-    //     console.log(response.data);
-    //     })
-
-    // axios.get('http://abdoking95.tplinkdns.com:3000/login', this.state)
-    //     .then(function (response) {
-    //         console.log(response);
-    //     })
-    //     .catch(function (error) {
-    //         console.log(error);
-    //     });
   }
 
   render() {
@@ -92,19 +48,8 @@ class welcomePage extends Component {
           <Navbar.Collapse className="justify-content-center" id="logo">
             <Navbar.Text className="navlink" href="#home">
               <Navbar.Brand href="#home">
-                <img
-                  alt=""
-                  src={Logo}
-                  width="40"
-                  height="40"
-                  className="mb-3"
-                />{" "}
-                <span className="navbarSpan">
-                  good
-                  <span className="navbarSpan" style={{ fontWeight: "bolder" }}>
-                    reads
-                  </span>
-                </span>
+                <img alt="" src={Logo} width="40" height="40" className="mb-3" />{" "}
+                <span className="navbarSpan">good<span className="navbarSpan" style={{ fontWeight: "bold" }}>reads</span></span>
               </Navbar.Brand>
             </Navbar.Text>
           </Navbar.Collapse>
@@ -113,60 +58,15 @@ class welcomePage extends Component {
           <div className="col-7 h-100 py-5" id="quote">
             <h1 className="mt-3">Your next book is our mission.</h1>
           </div>
-          <div
-            className="col-5 h-100 pt-3"
-            style={{ backgroundColor: "rgba(23, 23, 24, 0.24)" }}
-          >
-            {/* <Form className="form py-3 px-4 w-75" noValidate onSubmit={this.onSubmit}>
-                            <Form.Group controlId="formBasicEmail">
-                                <Form.Label>Email address</Form.Label>
-                                <Form.Control className="FormInput" type="email" placeholder="Enter email" name="mail" value={this.state.mail} onChange={this.onChange} />
-                            </Form.Group>
-
-                            <Form.Group controlId="formBasicPassword">
-                                <Form.Label>Password</Form.Label>
-                                <Form.Control className="FormInput" type="password" placeholder="Password" name="pass" value={this.state.pass} onChange={this.onChange} />
-                            </Form.Group>
-                            <div id="btn" className="text-center">
-                                <Button type="submit" style={{ backgroundColor: '#247BA0', borderColor: '#247BA0' }}>Login</Button>
-                            </div>
-                        </Form>
-                        <div className="signinIcons px-4 pt-2 mt-2 w-75">
-                            <span className="signinSpam">or sign in using</span>
-                            <a href=""><img alt="" src={FbSignin} width="27" height="27" className="ml-2" /></a>{'   '}
-                            <a href=""><img alt="" src={TwSignin} width="30" height="30" className="ml-2" /></a>{'   '}
-                            <a href=""><img alt="" src={GgleSignin} width="27" height="27" className="ml-2" /></a>{'   '}
-                            <br/>
-                            <div><a href="Registration ">3omar</a></div>
-                        </div> */}
-
-            <Tabs
-              defaultActiveKey="Login"
-              transition={false}
-              id="noanim-tab-example"
-              style={{ width: "40%", border: "transparent", paddingBottom:"7%",marginLeft:'18%' }}
-            >
+          <div className="col-5 h-100 pt-3" style={{ backgroundColor: "rgba(23, 23, 24, 0.24)" }} >
+            <Tabs defaultActiveKey="Login" transition={false} id="noanim-tab-example" style={{ width: "40%", border: "transparent", paddingBottom:"7%",marginLeft:'18%' }} >
               <Tab eventKey="Login" title="Login">
-           
-
-                <Form
-                  className="form w-75 rgba-stylish-strong z-depth-4"
-                  noValidate
-                  onSubmit={this.onSubmit}
-                >
+                <Form className="form w-75 rgba-stylish-strong z-depth-4" noValidate onSubmit={this.onSubmit}>
                     <div className="rgba-stylish-strong z-depth-4 py-3 px-4  " >
                   <Form.Group controlId="formBasicEmail">
                     <Form.Label>Email address</Form.Label>
-                    <Form.Control
-                      className="FormInput"
-                      type="email"
-                      placeholder="Enter email"
-                      name="mail"
-                      value={this.state.mail}
-                      onChange={this.onChange}
-                    />
+                    <Form.Control className="FormInput" type="email" placeholder="Enter email" name="mail" value={this.state.mail} onChange={this.onChange} />
                   </Form.Group>
-
                   <Form.Group controlId="formBasicPassword">
                     <Form.Label>Password</Form.Label>
                     <Form.Control
@@ -224,9 +124,6 @@ class welcomePage extends Component {
                   </a>
                   {"   "}
                   <br />
-                  <div>
-                    <a href="Registration ">3omar</a>
-                  </div>
                 </div>
               </Tab>
 
