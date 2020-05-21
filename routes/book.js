cfdb26d44e6fb937efb3d42aa2324a23ded9ad29
Omaure <omarakram958@gmail.com>
2020-05-21 23:55:18 +0200

    Adding Images to userbooks

--- routes/book.js
+++ routes/book.js
@@ -15,84 +15,82 @@ const checkJWT = require("../middlewares/jwt_auth")
 
 
 const storage = multer.diskStorage({
-   destination: function (req, file, callback) {
-      callback(null, './public/cover_imgs/')
-   },
-   filename: function (req, file, callback) {
-      callback(null, new Date().toISOString() + file.originalname);
-   }
+    destination: function (req, file, callback) {
+        callback(null, './public/cover_imgs/')
+    },
+    filename: function (req, file, callback) {
+        callback(null, new Date().toISOString() + file.originalname);
+    }
 });
 const upload = multer({
-   storage: storage
+    storage: storage
 });
-
-
 
 
 router.get('/', async (req, res) => {
 
-   try {
-      let results;
+    try {
+        let results;
 
-      
-      if (req.query.authorID && req.query.catID) {
-         results = BookModel.findBooksByCatAndAuthorId(req.query.catID,req.query.autherID)
 
-      }else if(req.query.authorID){
-         results = await BookModel.find({authorId:req.query.authorID})
-         console.log(req.query.authorID);
-         console.log(results);
-         
-         // results = BookModel.getBooksByAuthorID(req.query.autherID)
-      }else if(req.query.catID){
-         results = BookModel.findBooksByCatId(req.query.catID)
-         
-      }
+        if (req.query.authorID && req.query.catID) {
+            results = BookModel.findBooksByCatAndAuthorId(req.query.catID, req.query.autherID)
 
-      if (!results) {
-         results = await BookModel.getAllBooks();         
-      }
-      
-      res.json(results)
-   } catch (error) {
-      console.log(error);
-      res.status(404).send(error)
-   }
+        } else if (req.query.authorID) {
+            results = await BookModel.find({authorId: req.query.authorID})
+            console.log(req.query.authorID);
+            console.log(results);
+
+            // results = BookModel.getBooksByAuthorID(req.query.autherID)
+        } else if (req.query.catID) {
+            results = BookModel.findBooksByCatId(req.query.catID)
+
+        }
+
+        if (!results) {
+            results = await BookModel.getAllBooks();
+        }
+
+        res.json(results)
+    } catch (error) {
+        console.log(error);
+        res.status(404).send(error)
+    }
 
 });
 
 
 router.get('/:id', async (req, res) => {
-   try {
-      let id = req.params.id;
-      let results = await BookModel.findById(id).populate("authorId").populate("catId").exec()
-      let bookReviews = await ReviewModel.findReviewsByBookId(id)
-      console.log(bookReviews);
+    try {
+        let id = req.params.id;
+        let results = await BookModel.findById(id).populate("authorId").populate("catId").exec()
+        let bookReviews = await ReviewModel.findReviewsByBookId(id)
+        console.log(bookReviews);
 
-      let bookAvgRate=0;
-      let reviews =[]
+        let bookAvgRate = 0;
+        let reviews = []
 
-      if (bookReviews !== null) {
-         reviews = bookReviews.length >0 ? bookReviews[0]:[]         
+        if (bookReviews !== null) {
+            reviews = bookReviews.length > 0 ? bookReviews[0] : []
 
-         let avgRate=0;
-         if(bookReviews.length >0){
-            const bookTotalRate = bookReviews.reduce((total,review)=> total + review.rating,0)
-   
-            avgRate = bookTotalRate/bookReviews.length
-         }
-   
-         bookAvgRate =avgRate
-   
-      }      
+            let avgRate = 0;
+            if (bookReviews.length > 0) {
+                const bookTotalRate = bookReviews.reduce((total, review) => total + review.rating, 0)
 
-      res.json({book:results,bookAvgRate,reviews})
-   } catch (error) {
-      console.log(error);
-      res.send(404, {
-         error
-      })
-   }
+                avgRate = bookTotalRate / bookReviews.length
+            }
+
+            bookAvgRate = avgRate
+
+        }
+
+        res.json({book: results, bookAvgRate, reviews})
+    } catch (error) {
+        console.log(error);
+        res.send(404, {
+            error
+        })
+    }
 });
 
 router.use(checkJWT)
@@ -100,54 +98,54 @@ router.use(checkIsAdmin)
 
 router.post('/', upload.single('coverImage'), async function (req, res) {
 
-   try {
+    try {
 
-      
-      // Here we need to check the JWT token before creating a new book
-      // const newAuthor = new AuthorModel()
-      // const newCat = new CategoryModel()
-      const newBook = new BookModel({
-         bookName: req.body.bookName,
-         catId: req.body.catId,
-         authorId: req.body.authorId,
-         coverImageName: req.file.path,
-         brief:req.body.brief
-      });
-      const book = await newBook.save();
-      res.status(201).json(book);
-   } catch (error) {
-      console.log(error);
-      res.sendStatus(409);
-   }
 
-});
-router.delete('/:id', async(req, res) => {
-   try {
-      let id = req.params.id;
-      let results = await BookModel.findByIdAndDelete(id).exec()
-      res.json(results)
-   } catch (error) {
-      console.log(error);
-      res.send(404, {
-         error
-      })
-   }
+        // Here we need to check the JWT token before creating a new book
+        // const newAuthor = new AuthorModel()
+        // const newCat = new CategoryModel()
+        const newBook = new BookModel({
+            bookName: req.body.bookName,
+            catId: req.body.catId,
+            authorId: req.body.authorId,
+            coverImageName: req.file.path,
+            brief: req.body.brief
+        });
+        const book = await newBook.save();
+        res.status(201).json(book);
+    } catch (error) {
+        console.log(error);
+        res.sendStatus(409);
+    }
 
 });
+router.delete('/:id', async (req, res) => {
+    try {
+        let id = req.params.id;
+        let results = await BookModel.findByIdAndDelete(id).exec()
+        res.json(results)
+    } catch (error) {
+        console.log(error);
+        res.send(404, {
+            error
+        })
+    }
 
-router.patch('/:id',upload.single('coverImage') ,async(req, res) => {
-   try {
-      let id = req.params.id;
-      console.log(req.body);
-      
-      let results = await BookModel.findByIdAndUpdate(id,req.body,{new:true}).exec()
-      res.json(results)
-   } catch (error) {
-      console.log(error);
-      res.send(404, {
-         error
-      })
-   }
+});
+
+router.patch('/:id', upload.single('coverImage'), async (req, res) => {
+    try {
+        let id = req.params.id;
+        console.log(req.body);
+
+        let results = await BookModel.findByIdAndUpdate(id, req.body, {new: true}).exec()
+        res.json(results)
+    } catch (error) {
+        console.log(error);
+        res.send(404, {
+            error
+        })
+    }
 });
 
 
