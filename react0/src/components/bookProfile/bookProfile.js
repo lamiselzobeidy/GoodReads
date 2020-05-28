809a92b26183ec93106635421e544f6b0fcef030
Lamis Elzobeidy <lamis.zobeidy@gmail.com>
2020-05-28 05:32:42 +0200

    added review, rating and book change state

--- react0/src/components/bookProfile/bookProfile.js
+++ react0/src/components/bookProfile/bookProfile.js
@@ -1,48 +1,153 @@
 import React, { useState } from 'react';
 import './bookProfile.css';
-import { Row, Col, Dropdown, Card } from 'react-bootstrap';
+import { Row, Col, Dropdown, Card, ListGroup, InputGroup, FormControl, Button } from 'react-bootstrap';
 import { MDBRating } from 'mdbreact';
 import axios from 'axios';
 import { Link } from 'react-router-dom';
 import BeautyStars from 'beauty-stars';
 
+
+
 const BookProfile = (props) => {
     const { match: { params } } = props;
-    console.log(params);
+    console.log(JSON.parse(sessionStorage.getItem('user')).token)
+    const [review, setReview] = useState("");
     const [book, setBook] = useState(0);
-    const [stars, setSars] = useState(0);
+    const [stars, setStars] = useState(0);
     const getBook = () => {
-        axios.get(`http://34.107.102.252:3000/book/${params.bookId}`)
-        // axios.get('http://34.107.102.252:3000/book/5ec2d02f919f0b7d7211addf')
+        axios.get(`http://34.107.102.252:3000/book/${params.bookId}`, { headers: { "JWT": `${JSON.parse(sessionStorage.getItem('user')).token}` } })
             .then(result => {
                 setBook(result.data);
                 console.log(result.data)
+                setStars(result.data.userData.userRating)
             })
+
     }
+
+    const changeBookStatus = (bookId, operation) => {
+        axios({
+            method: 'patch',
+            url: `http://34.107.102.252:3000/user/book/change/${bookId}`,
+            data: { operation: operation },
+            headers: { 'JWT': JSON.parse(sessionStorage.getItem("user")).token }
+        }).then(result => {
+            console.log("done axios");
+        });
+    }
+
+    const onChange = (e) => {
+        setReview(e.target.value)
+    }
+
+
     if (book) {
         return (
             <div className=" pt-5 bookDetails">
                 <Row className="mx-auto">
                     <Col xs="4">
                         <Card className="bookCard mx-auto" >
-                            <Card.Img variant="top" src={`http://34.107.102.252:3000/`+book.book.coverImageName} height="120px" className="w-75 mx-auto mt-3" />
+                            <Card.Img variant="top" src={`http://34.107.102.252:3000/` + book.book.coverImageName} height="120px" className="w-75 mx-auto mt-3" />
                             <Card.Body className="mx-auto">
                                 <Dropdown>
                                     <Dropdown.Toggle variant="info" id="dropdown-basic" style={{ fontSize: '12px' }}>Want to Read</Dropdown.Toggle>
                                     <Dropdown.Menu >
-                                        <Dropdown.Item href="#/action-1" style={{ fontSize: '12px' }}>Want to Read</Dropdown.Item>
-                                        <Dropdown.Item href="#/action-2" style={{ fontSize: '12px' }}>Currently reading</Dropdown.Item>
-                                        <Dropdown.Item href="#/action-3" style={{ fontSize: '12px' }}>Done</Dropdown.Item>
+                                        <Dropdown.Item href="#/action-1" style={{ fontSize: '12px' }}
+                                            onClick={() => {
+                                                if (book.userData.userBookStatus == "read") {
+                                                    changeBookStatus(book.book._id, 6)
+                                                }
+                                                else if (book.userData.userBookStatus == "current") {
+                                                    changeBookStatus(book.book._id, 2)
+                                                }
+                                                else if (book.userData.userBookStatus == "want") {
+                                                    console.log("la2");
+                                                }
+                                                else if (book.userData.userBookStatus == "") {
+                                                    axios({
+                                                        method: 'post',
+                                                        url: `http://34.107.102.252:3000/user/book/want/${book.book._id}`,
+                                                        headers: { 'JWT': JSON.parse(sessionStorage.getItem("user")).token }
+                                                    })
+                                                }
+                                            }}
+                                        >Want to Read</Dropdown.Item>
+                                        <Dropdown.Item href="#/action-2" style={{ fontSize: '12px' }}
+                                            onClick={() => {
+                                                if (book.userData.userBookStatus == "read") {
+                                                    changeBookStatus(book.book._id, 5)
+                                                }
+                                                else if (book.userData.userBookStatus == "current") {
+                                                    console.log("la2");
+                                                }
+                                                else if (book.userData.userBookStatus == "want") {
+                                                    changeBookStatus(book.book._id, 3)
+                                                }
+                                                else if (book.userData.userBookStatus == "") {
+                                                    // axios.post(`http://34.107.102.252:3000/user/book/current/${book.book._id}`, {headers: { 'JWT': JSON.parse(sessionStorage.getItem("user")).token } })
+                                                    console.log(book.book._id);
+                                                    axios({
+                                                        method: 'post',
+                                                        url: `http://34.107.102.252:3000/user/book/current/${book.book._id}`,
+                                                        headers: { 'JWT': JSON.parse(sessionStorage.getItem("user")).token }
+                                                    })
+                                                }
+                                            }}
+                                        >Currently reading</Dropdown.Item>
+                                        <Dropdown.Item href="#/action-3" style={{ fontSize: '12px' }}
+                                            onClick={() => {
+                                                if (book.userData.userBookStatus == "read") {
+                                                    console.log("la2");
+                                                }
+                                                else if (book.userData.userBookStatus == "current") {
+                                                    changeBookStatus(book.book._id, 1)
+                                                }
+                                                else if (book.userData.userBookStatus == "want") {
+                                                    changeBookStatus(book.book._id, 4)
+                                                }
+                                                else if (book.userData.userBookStatus == "") {
+                                                    axios({
+                                                        method: 'post',
+                                                        url: `http://34.107.102.252:3000/user/book/read/${book.book._id}`,
+                                                        headers: { 'JWT': JSON.parse(sessionStorage.getItem("user")).token }
+                                                    })
+                                                }
+                                            }}
+                                        >Done reading</Dropdown.Item>
                                     </Dropdown.Menu>
                                 </Dropdown>
-                                <BeautyStars size="20px" value={stars} onChange={value => setSars(value)} />
+                                <BeautyStars size="20px" value={stars} onChange={
+                                    value => {
+                                        if (!book.userData.userRating && book.userData.userReview == "") {
+                                            axios({
+                                                method: 'post',
+                                                url: `http://34.107.102.252:3000/user/book/review`,
+                                                data: {
+                                                    bookId: `${book.book._id}`,
+                                                    rating: value
+                                                },
+                                                headers: { 'JWT': JSON.parse(sessionStorage.getItem("user")).token }
+                                            })
+                                        }
+                                        else {
+                                            axios({
+                                                method: 'patch',
+                                                url: `http://34.107.102.252:3000/user/book/review`,
+                                                data: {
+                                                    bookId: `${book.book._id}`,
+                                                    rating: value
+                                                },
+                                                headers: { 'JWT': JSON.parse(sessionStorage.getItem("user")).token }
+                                            })
+                                        }
+                                        setStars(value)
+                                    }} />
                             </Card.Body>
                         </Card>
 
                     </Col>
                     <Col xs="5" className="pt-4 bookInfo" >
                         <h2 className="bookname">{book.book.bookName}</h2>
-                        <Link>by {book.book.authorId.firstName} {book.book.authorId.lastName}</Link><br/>
+                        <Link>by {book.book.authorId.firstName} {book.book.authorId.lastName}</Link><br />
                         <Link>{book.book.catId.categoryName}</Link>
                         <BeautyStars size="15px" value={book.bookAvgRate} />
                         <p className="mt-3">{book.book.brief}</p>
@@ -52,8 +157,107 @@ const BookProfile = (props) => {
                 <Row className="justify-content-md-center w-100">
                     <Col sm={10}>
                         <div className="mt-5 pt-3">
-                            <h2 className="bookname">Review</h2>
-                            <p>First Review: Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+                            <h2 className="bookname">Reviews</h2>
+                            <ListGroup variant="flush">
+                                {book.reviews.map(review => (
+                                    <ListGroup.Item key={review._id}><span style={{ color: "#b462cb", fontWeight: "bold" }}>{review.userId.firstName} {review.userId.lastName}</span> : {review.review}</ListGroup.Item>
+                                ))}
+                            </ListGroup>
+                            <InputGroup className="mb-3">
+                                <FormControl
+                                    placeholder="add your review.."
+                                    aria-label="username"
+                                    aria-describedby="basic-addon2"
+                                    as="textarea"
+                                    rows="4"
+                                    value={review}
+                                    onChange={onChange}
+                                />
+                                <InputGroup >
+                                    <Button onClick={() => {
+                                        if (book.userData.userRating || book.userData.userReview !== "") {
+                                            console.log(review + stars);
+                                            if (!stars && review !== "") {
+                                                axios({
+                                                    method: 'patch',
+                                                    url: `http://34.107.102.252:3000/user/book/review`,
+                                                    data: {
+                                                        bookId: `${book.book._id}`,
+                                                        review: `${review}`
+                                                    },
+                                                    headers: { 'JWT': JSON.parse(sessionStorage.getItem("user")).token }
+                                                })
+                                            }
+                                            else if (review === "" && stars) {
+                                                axios({
+                                                    method: 'patch',
+                                                    url: `http://34.107.102.252:3000/user/book/review`,
+                                                    data: {
+                                                        bookId: `${book.book._id}`,
+                                                        rating: stars
+                                                    },
+                                                    headers: { 'JWT': JSON.parse(sessionStorage.getItem("user")).token }
+                                                })
+                                            }
+                                            else if (review === "" && !stars) {
+                                                console.log("didn't add review nor rating");
+                                            }
+                                            else {
+                                                axios({
+                                                    method: 'patch',
+                                                    url: `http://34.107.102.252:3000/user/book/review`,
+                                                    data: {
+                                                        bookId: `${book.book._id}`,
+                                                        rating: stars,
+                                                        review: `${review}`
+                                                    },
+                                                    headers: { 'JWT': JSON.parse(sessionStorage.getItem("user")).token }
+                                                })
+                                            }
+                                        }
+                                        else {
+                                            console.log("fl else");
+                                            if (!stars && review !== "") {
+                                                axios({
+                                                    method: 'post',
+                                                    url: `http://34.107.102.252:3000/user/book/review`,
+                                                    data: {
+                                                        bookId: `${book.book._id}`,
+                                                        review: `${review}`
+                                                    },
+                                                    headers: { 'JWT': JSON.parse(sessionStorage.getItem("user")).token }
+                                                })
+                                            }
+                                            else if (review === "" && stars) {
+                                                axios({
+                                                    method: 'post',
+                                                    url: `http://34.107.102.252:3000/user/book/review`,
+                                                    data: {
+                                                        bookId: `${book.book._id}`,
+                                                        rating: stars
+                                                    },
+                                                    headers: { 'JWT': JSON.parse(sessionStorage.getItem("user")).token }
+                                                })
+                                            }
+                                            else if (review === "" && !stars) {
+                                                console.log("didn't add review nor rating");
+                                            }
+                                            else {
+                                                axios({
+                                                    method: 'post',
+                                                    url: `http://34.107.102.252:3000/user/book/review`,
+                                                    data: {
+                                                        bookId: `${book.book._id}`,
+                                                        rating: stars,
+                                                        review: `${review}`
+                                                    },
+                                                    headers: { 'JWT': JSON.parse(sessionStorage.getItem("user")).token }
+                                                })
+                                            }
+                                        }
+                                    }} variant="outline-secondary" className="mt-4">Add</Button>
+                                </InputGroup>
+                            </InputGroup>
                         </div>
                     </Col>
                 </Row>
